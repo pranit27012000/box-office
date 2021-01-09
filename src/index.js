31db97f07704ff01e55e44968c4c9a6df1de8dc6
pranit <pranit27012000>
2021-01-10 00:40:36 +0530

    changes done

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom';
+import {HashRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<BrowserRouter>
+ReactDOM.render(<HashRouter>
                     <App />
-                    </BrowserRouter>,
+                    </HashRouter>,
 
   document.getElementById('root')
 );
